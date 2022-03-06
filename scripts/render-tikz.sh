66716e8438c06723f40388c4771502edddb308fb
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-03-06 18:39:45 +0000

    only commit tex,pdf,svg,png files, install texlive-latex-extra for package standalone

--- scripts/render-tikz.sh
+++ scripts/render-tikz.sh
@@ -9,25 +9,29 @@ dir=${1%/*}
 # Get basepath of TeX file, i.e. path without extension.
 basepath=${1%.tex}
 
-# Create PDF from TeX file.
+echo "Running latexmk to generate PDF from TeX file"
 ret_val=$(latexmk -silent -pdf -jobname="$basepath" "$1")
 
-if [ "$CI" = true ] && [ "$ret_val" -eq 0 ]; then
-    echo "PDF created successfully."
-else
+if [ "$CI" = true ]; then
+  echo "Detected CI=$CI"
+  if [ "$ret_val" -ne 0 ]; then
+    echo "latexmk failed with return code $ret_val"
     cat "$basepath.log"
+    exit 1
+  fi
 fi
 
 
-# Delete LaTeX auxiliary files.
+echo "Delete LaTeX auxiliary files"
 find "$dir" -type f -regex ".*\.(aux|log|fls|fdb_latexmk)$" -delete
 
-# Compress PDF. iLovePDF performs much better than Ghostscript but the API only offers
+echo "Compressing output PDF with pdf-compressor"
+# iLovePDF performs much better than Ghostscript but the API only offers
 # 250 free compressions per month.
 # scripts/gs-pdf-compress.sh "$dir"
 pdf-compressor --inplace "$basepath.pdf"
 
-# Convert PDF to SVG and compress.
+echo "Converting PDF to SVG and compressing"
 # Requires pdf2svg and svgo to be installed (`brew install pdf2svg svgo`).
 pdf2svg "$basepath.pdf" "$basepath.svg"
 # If the SVG is larger than 1 MB, delete it instead of compressing.
@@ -39,8 +43,13 @@ else
   svgo --multipass "$basepath.svg"
 fi
 
-# Convert PDF to PNG at low and high resolution, then compress both. Requires convert,
-# pngquant and zopflipng to be installed (`brew install imagemagick pngquant zopfli`).
+# https://stackoverflow.com/q/52998331
+if [ "$CI" = true ]; then
+  sed -i '/disable ghostscript format types/,+6d' /etc/ImageMagick-6/policy.xml
+fi
+echo "Converting PDF to low + high res PNG, then compressing both"
+# Requires convert, pngquant and zopflipng to be installed:
+# brew install imagemagick pngquant zopfli
 convert -density 200 "$basepath.pdf" "$basepath.png"
 convert -density 400 "$basepath.pdf" "$basepath-hd.png"
 
