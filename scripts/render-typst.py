4736342db58e8f87fed11292e8e56e2e394e5c54
Janosh Riebesell <janosh.riebesell@gmail.com>
2024-04-09 15:34:57 +0200

    add render-typst.py script using new pdf_to_svg_png_compressed() in scripts/__init__.py

---
+++ scripts/render-typst.py
@@ -0,0 +1,22 @@
+import os
+import runpy
+import subprocess
+import sys
+
+sys.path.append(".")
+
+from scripts import pdf_to_svg_png_compressed
+
+# Get name of directory containing the Typst file.
+input_file = sys.argv[1]
+
+# Get base path of Typst file, i.e. path without extension.
+base_path = os.path.splitext(input_file)[0]
+
+print("Generating PDF from Typst file")
+ret_val = subprocess.run(["typst", "compile", input_file]).returncode
+
+pdf_to_svg_png_compressed(f"{base_path}.pdf")
+
+print("Update readme table listing all figures in assets/")
+runpy.run_path("scripts/update-readme-table.py")
